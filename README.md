e432f3e5d2e74f980f94812a491ab00e71d42df7
unknown <david.csierra@labinfo.is.escuelaing.edu.co>
2025-08-16 09:52:56 -0500

    entrega inicial

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # lab1-arsw
+
+## Nombre: David Santiago Castro
+
+
+![img.png](img/img.png)
+
+
+Para este punto desarrollamos el siguiente codigo en CountThread:
+
+![img.png](img/img2.png)
+
+![img.png](img/img3.png)
+
+Y lo probamos en CountThreadMain:
+
+![img.png](img/img4.png)
+
+Vemos que el orden de ejecucion de los hilos no esta garantizado por lo que los numeros se imprimen en cualquier orden
+
+![img.png](img/img5.png)
+
+![img.png](img/img6.png)
+
+Lo que esta sucediendo es que se esta ejecuntando de forma secuecial como si no hubieran hilos lo cual hace que no se ejecuten en paralelo
+
+![img.png](img/img7.png)
---
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+
+import java.sql.SQLOutput;
+
+/**
+ *
+ * @author David Castro
+ */
+public class CountThread extends Thread {
+    private final int a, b;
+
+    CountThread(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public void run() {
+        for (int i = a+1; i < b; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+}
